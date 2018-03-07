ec74627b5a514928d3f1ab7b506f1e9a6c07964f
mswift42 <martin.haesler@gmail.com>
2018-03-07 16:31:36 +0000

    add test for films1.html page

--- tv/tv_test.go
+++ tv/tv_test.go
@@ -11,5 +11,13 @@ func TestLoadingDocument(t *testing.T) {
 	if idr.idoc.doc == nil {
 		t.Error("Expected idoc not to be nil", idr.idoc)
 	}
+	url = TestHtmlUrl("testhtml/films1.html")
+	idr = url.loadDocument()
+	if idr.Error != nil {
+		t.Error("Expected error to be nil: ", idr.Error)
+	}
+	if idr.idoc.doc == nil {
+		t.Error("Expected idoc not to be nil: ", idr.idoc)
+	}
 }
 
