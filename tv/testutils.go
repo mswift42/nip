33b2e31e428c18d7666044d6ec8b4ee92c706b4b
mswift42 <martin.haesler@gmail.com>
2018-03-30 08:21:56 +0100

    delete newMainCategory method

--- tv/testutils.go
+++ tv/testutils.go
@@ -9,31 +9,16 @@ import (
 
 type TestHtmlUrl string
 
-func (thu TestHtmlUrl) newMainCategory() *mainCategoryDocument {
-	var results []*iplayerDocument
-	maindocres := thu.loadDocument()
-	if maindocres.Error != nil {
-		return &mainCategoryDocument{ nil, results}
-	}
-	np := maindocres.idoc.nextPages()
-	npresults := collectNextPages(np)
-	for _, i := range npresults {
-		if i.Error == nil {
-			results = append(results, &i.idoc)
-		}
-	}
-	return &mainCategoryDocument{&maindocres.idoc, results}
-}
 
 
-func (thu TestHtmlUrl) loadDocument(c chan<- *iplayerDocumentResult) *iplayerDocumentResult {
+func (thu TestHtmlUrl) loadDocument(c chan<- *iplayerDocumentResult) {
 	file, err := ioutil.ReadFile(string(thu))
 	if err != nil {
 		c <-  &iplayerDocumentResult{iplayerDocument{}, err}
 	}
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
 	if err != nil {
-		c <- return &iplayerDocumentResult{iplayerDocument{}, err}
+		c <- &iplayerDocumentResult{iplayerDocument{}, err}
 	}
 	idoc := iplayerDocument{doc}
 	c <-  &iplayerDocumentResult{idoc, nil}
