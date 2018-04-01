88cbface9aef265c538e548d207be90d7aa21cc3
mswift42 <martin.haesler@gmail.com>
2018-04-01 17:56:43 +0100

    Add collectPages method for testhtmlurl

--- tv/testutils.go
+++ tv/testutils.go
@@ -9,17 +9,30 @@ import (
 
 type TestHtmlUrl string
 
-
-
 func (thu TestHtmlUrl) loadDocument(c chan<- *iplayerDocumentResult) {
 	file, err := ioutil.ReadFile(string(thu))
 	if err != nil {
-		c <-  &iplayerDocumentResult{iplayerDocument{}, err}
+		c <- &iplayerDocumentResult{iplayerDocument{}, err}
 	}
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
 	if err != nil {
 		c <- &iplayerDocumentResult{iplayerDocument{}, err}
 	}
 	idoc := iplayerDocument{doc}
-	c <-  &iplayerDocumentResult{idoc, nil}
+	c <- &iplayerDocumentResult{idoc, nil}
+}
+
+func (thu TestHtmlUrl) collectPages(urls []string) []*iplayerDocumentResult {
+	var results []*iplayerDocumentResult
+	c := make(chan *iplayerDocumentResult)
+	for _, i := range urls {
+		go func(s string) {
+			thu := TestHtmlUrl(s)
+			thu.loadDocument(c)
+		}(i)
+	}
+	for i := 0; i < len(urls); i++ {
+		results = append(results, <-c)
+	}
+	return results
 }
