765627349fdd875a3edc96f69e8c751015b4a28d
mswift42 <martin.haesler@gmail.com>
2018-04-20 06:54:03 +0100

    Fix test for thumbnail.

--- tv/tv_test.go
+++ tv/tv_test.go
@@ -113,7 +113,7 @@ func TestProgramPage(t *testing.T) {
 	if p1.URL != "/iplayer/episode/b09yn368/classic-mary-berry-series-1-episode-6" {
 		t.Error("Expected url to '/iplayer/episode/b09yn368/classic-mary-berry-series-1-episode-6', got: ", p1.URL)
 	}
-	if p1.Thumbnail != "https://ichef.bbci.co.uk/images/ic/784x441/p05yp36d.jpg" {
+	if p1.Thumbnail != "https://ichef.bbci.co.uk/images/ic/304x171/p062dlmz.jpg" {
 		t.Error("Expected imageurl to be 'https://ichef.bbci.co.uk/images/ic/784x441/p05yp36d.jpg', got: ", p1.Thumbnail)
 	}
 }
