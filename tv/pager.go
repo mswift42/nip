eead47eb142a376f09306c9cd63de0c548855ed4
mswift42 <martin.haesler@gmail.com>
2018-05-01 14:09:25 +0100

    change programPages signature to take iplayerDocumentResults as arg.

--- tv/pager.go
+++ tv/pager.go
@@ -7,5 +7,5 @@ type Pager interface {
 type NextPager interface {
 	mainDoc() *iplayerDocument
 	nextPages() []Pager
-	programPages() ([]Pager, []*iplayerSelectionResult)
+	programPages([]*iplayerDocumentResult) ([]Pager, []*iplayerSelectionResult)
 }
