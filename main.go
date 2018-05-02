48c3a5d2a3931e0d47b830897169e0f9b9011460
mswift42 <martin.haesler@gmail.com>
2018-05-02 17:21:05 +0100

    import package tv.

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/mswift42/nip/tv"
+	"fmt"
+	tv2 "github.com/mswift42/ipn/tv"
+)
 
 func main() {
-	fmt.Println("running")
+	url := "https://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz&page=1"
+	bu := tv.BeebURL(url)
+	fmt.Println(bu)
+	c := make(chan *tv2.IplayerDocumentResult)
+	go bu
 }
