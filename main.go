a670985e9c2c5415c9a31a6b55b134a2f4546d98
mswift42 <martin.haesler@gmail.com>
2018-05-05 14:03:16 +0100

    print remote programme title.

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/mswift42/nip/tv"
 	"fmt"
-	tv2 "github.com/mswift42/ipn/tv"
 )
 
 func main() {
 	url := "https://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz&page=1"
-	bu := tv.BeebURL(url)
-	fmt.Println(bu)
-	c := make(chan *tv2.IplayerDocumentResult)
-	go bu
+	doc := tv.RemoteDocumentLoader(url)
+	nmc := tv.NewMainCategory(doc)
+	progs := nmc.Programmes()
+	for _, i := range progs {
+		fmt.Println(i.Title)
+	}
 }
