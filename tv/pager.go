029d3d2bb76e18059173c5de1748e9faf06671b1
mswift42 <martin.haesler@gmail.com>
2018-05-08 11:21:21 +0100

    Change signature of programPages.

--- tv/pager.go
+++ tv/pager.go
@@ -7,5 +7,5 @@ type Pager interface {
 type NextPager interface {
 	mainDoc() *iplayerDocument
 	nextPages() []Pager
-	programPages([]*iplayerDocument) ([]Pager, []*iplayerSelectionResult)
+	programPages([]*iplayerSelectionResult) []Pager
 }
