bfa149b7c559632494333b534363f8ac87a346a8
mswift42 <martin.haesler@gmail.com>
2018-05-18 06:16:27 +0100

    Update TODO's.

--- tv/tv.go
+++ tv/tv.go
@@ -295,7 +295,6 @@ func NewMainCategory(np NextPager) *MainCategoryDocument {
 //	return results
 //}
 
-// TODO - Check if loadDocument should be using a map[pager]iplayerdocumentresult
 
 var seen = make(map[Pager]*IplayerDocumentResult)
 var mutex = &sync.Mutex{}
