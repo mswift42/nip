e24691ef7144fb84a01bca6e744caf68732d82bb
mswift42 <martin.haesler@gmail.com>
2018-05-28 06:36:58 +0100

    fix tv imports

---
+++ tv/db.go
@@ -0,0 +1,100 @@
+package tv
+
+import (
+	"time"
+	"encoding/json"
+	"io/ioutil"
+	"bytes"
+	"fmt"
+	"github.com/pkg/errors"
+	"strings"
+)
+
+type ProgrammeDB struct {
+	Categories []*Category `json:"categories"`
+	Saved time.Time `json:"saved"`
+}
+
+func RestoreProgrammeDB(filename string) (*ProgrammeDB, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var pdb *ProgrammeDB
+	json.Unmarshal(file, pdb)
+	return pdb, nil
+}
+
+func (pdb *ProgrammeDB) toJSON() ([]byte, error) {
+	marshalled, err := json.MarshalIndent(pdb, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+	return marshalled, err
+}
+
+func (pdb *ProgrammeDB) Save(filename string) error {
+	pdb.Saved = time.Now()
+	pdb.index()
+	enc,err := pdb.toJSON()
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, enc, 0644)
+}
+
+func (pdb *ProgrammeDB) index() {
+	index := 0
+	for _, i := range pdb.Categories {
+		for _, j := range i.Programmes {
+			j.Index = index
+			index++
+		}
+	}
+}
+
+func (pdb *ProgrammeDB) ListCategory(category string) string {
+	var buffer bytes.Buffer
+	cat, err := pdb.findCategory(category)
+	if err != nil {
+		return fmt.Sprintln(err)
+	}
+	for _, i := range cat.Programmes {
+		buffer.WriteString(i.String())
+		buffer.WriteString("\n")
+	}
+	return buffer.String()
+}
+
+func (pdb *ProgrammeDB) findCategory(category string) (*Category, error) {
+	for _, i := range pdb.Categories {
+		if i.Name ==  category {
+			return i, nil
+		}
+	}
+	return nil, errors.New("Can't find Category with Name: " + category)
+}
+
+func (pdb *ProgrammeDB) ListAvailableCategories() string {
+	var buffer bytes.Buffer
+	for _, i := range pdb.Categories {
+		buffer.WriteString(i.Name + "\n")
+	}
+	return buffer.String()
+}
+
+func (pdb *ProgrammeDB) FindTitle(title string) string {
+	var buffer bytes.Buffer
+	for _, i := range pdb.Categories {
+		for _, j := range i.Programmes {
+			if strings.Contains(strings.ToLower(j.String()),
+			strings.ToLower(title)) {
+				buffer.WriteString(j.String() + "\n")
+			}
+		}
+	}
+	if len(buffer.Bytes()) == 0 {
+		return "No Matches found.\n"
+	}
+	return buffer.String()
+}
---
+++ tv/db_test.go
@@ -0,0 +1,11 @@
+package tv
+
+import (
+	"testing"
+)
+
+func TestProgrammeDB_Save(t *testing.T) {
+	doc := DocumentLoader("../tv/testhtml/films1.html")
+	td := TestIplayerDocument{doc}
+	nmc
+}
