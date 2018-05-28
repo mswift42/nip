d923dfc49e0618d42a4b6f2f56a4283f882323f9
mswift42 <martin.haesler@gmail.com>
2018-05-28 14:58:21 +0100

    Check Saving db doesn't return error.

--- tv/db_test.go
+++ tv/db_test.go
@@ -2,10 +2,16 @@ package tv
 
 import (
 	"testing"
+	"time"
 )
 
 func TestProgrammeDB_Save(t *testing.T) {
-	doc := documentLoader("../tv/testhtml/films1.html")
+	doc := documentLoader("testhtml/films1.html")
 	td := &TestIplayerDocument{doc}
 	cat := NewCategory("films", td)
+	pdb := &ProgrammeDB{[]*Category{cat}, time.Now()}
+	err := pdb.Save("testdb.json")
+	if err != nil {
+		t.Error("Expected saving db should not return error, got: ", err)
+	}
 }
