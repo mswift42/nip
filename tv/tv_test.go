27c5dd43dcccf64fb452f00745f3586cbdf59818
mswift42 <martin.haesler@gmail.com>
2018-05-28 06:30:57 +0100

    fix category tests.

--- tv/tv_test.go
+++ tv/tv_test.go
@@ -507,10 +507,10 @@ func TestCategory(t *testing.T) {
 	doc := DocumentLoader("testhtml/films1.html")
 	td := TestIplayerDocument{doc}
 	cat := NewCategory("films", &td)
-	if cat.name != "films" {
-		t.Errorf("Expected category's name to be 'films' , got: %q", cat.name)
+	if cat.Name != "films" {
+		t.Errorf("Expected category's name to be 'films' , got: %q", cat.Name)
 	}
-	if len(cat.programmes) != 26 {
-		t.Error("Expected length of programmes to be 26, got: ", len(cat.programmes))
+	if len(cat.Programmes) != 26 {
+		t.Error("Expected length of programmes to be 26, got: ", len(cat.Programmes))
 	}
 }
