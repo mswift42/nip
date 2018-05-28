f4f34d66d2768698f8cb060a58979f383c507bc9
mswift42 <martin.haesler@gmail.com>
2018-05-28 16:07:22 +0100

    Add test for index method.

--- tv/db_test.go
+++ tv/db_test.go
@@ -5,6 +5,24 @@ import (
 	"time"
 )
 
+func TestProgrammeDB_Index(t *testing.T) {
+	doc := documentLoader("testhtml/films1.html")
+	td := &TestIplayerDocument{doc}
+	cat := NewCategory("films", td)
+	pdb := &ProgrammeDB{[]*Category{cat}, time.Now()}
+	pdb.index()
+	fp := pdb.Categories[0].Programmes[0]
+	if fp.Index != 0 {
+		t.Errorf("Expected first programme to have index '0', got: %d", fp.Index)
+	}
+	for _, i := range pdb.Categories[0].Programmes[1:] {
+		if !(i.Index > 0) {
+			t.Errorf("Expected for title %q index to be '> 0', got: %d", i.Title, i.Index)
+		}
+	}
+
+}
+
 func TestProgrammeDB_Save(t *testing.T) {
 	doc := documentLoader("testhtml/films1.html")
 	td := &TestIplayerDocument{doc}
