0e8b2254d97e0d0ee72e58a1a8ae466eb84f67a8
mswift42 <martin.haesler@gmail.com>
2018-05-31 06:50:30 +0100

    Add test for loadCategories.

--- tv/categories.go
+++ tv/categories.go
@@ -1,14 +1,14 @@
 package tv
 
-func loadCategory(name string, np *NextPager, c chan<- *Category) {
+func loadCategory(name string, np NextPager, c chan<- *Category) {
 	c <- NewCategory(name, np)
 }
 
-func loadCategories(catmap map[string]*NextPager) []*Category {
+func loadCategories(catmap map[string]NextPager) []*Category {
 	var cats []*Category
 	c := make(chan *Category)
 	for n, np := range catmap {
-		go func(name string, np *NextPager) {
+		go func(name string, np NextPager) {
 			loadCategory(name, np, c)
 		}(n, np)
 	}
