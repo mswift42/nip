2d6d04caa0b17fe036c96d65e4e7ad38be274ea5
mswift42 <martin.haesler@gmail.com>
2018-06-02 16:04:47 +0100

    Add var caturls.

--- tv/categories.go
+++ tv/categories.go
@@ -5,6 +5,14 @@ type Category struct {
 	Programmes []*Programme
 }
 
+var caturls = map[string]string{
+	"films" : "https://www.bbc.co.uk/iplayer/categories/films/a-z?sort=atoz&page=1",
+	"food" : "https://www.bbc.co.uk/iplayer/categories/food/a-z?sort=atoz&page=1",
+	"comedy" : "https://www.bbc.co.uk/iplayer/categories/comedy/a-z?sort=atoz",
+	"crime" : "https://www.bbc.co.uk/iplayer/categories/drama-crime/a-z?sort=atoz",
+
+}
+
 func newCategory(name string, np NextPager) *Category {
 	nmc := NewMainCategory(np)
 	return &Category{name, nmc.Programmes()}
