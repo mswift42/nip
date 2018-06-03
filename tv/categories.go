82b11b77b0c0298c0b1d30c75003f3a53746a21f
mswift42 <martin.haesler@gmail.com>
2018-06-03 11:57:55 +0100

    Add remaining Categories.

--- tv/categories.go
+++ tv/categories.go
@@ -10,7 +10,17 @@ var caturls = map[string]string{
 	"food" : "https://www.bbc.co.uk/iplayer/categories/food/a-z?sort=atoz&page=1",
 	"comedy" : "https://www.bbc.co.uk/iplayer/categories/comedy/a-z?sort=atoz",
 	"crime" : "https://www.bbc.co.uk/iplayer/categories/drama-crime/a-z?sort=atoz",
-
+	"classic&period" : "https://www.bbc.co.uk/iplayer/categories/drama-classic-and-period/a-z?sort=atoz",
+	"sc-fi&fantasy" : "https://www.bbc.co.uk/iplayer/categories/drama-sci-fi-and-fantasy/a-z?sort=atoz",
+	"documentaries" : "https://www.bbc.co.uk/iplayer/categories/documentaries/a-z?sort=atoz",
+	"arts" : "https://www.bbc.co.uk/iplayer/categories/arts/a-z?sort=atoz",
+	"entertainment" : "https://www.bbc.co.uk/iplayer/categories/entertainment/a-z?sort=atoz",
+	"history" : "https://www.bbc.co.uk/iplayer/categories/history/a-z?sort=atoz",
+	"lifestyle" : "https://www.bbc.co.uk/iplayer/categories/lifestyle/a-z?sort=atoz",
+	"music" : "https://www.bbc.co.uk/iplayer/categories/music/a-z?sort=atoz",
+	"news"  : "https://www.bbc.co.uk/iplayer/categories/news/a-z?sort=atoz",
+	"science&nature" : "https://www.bbc.co.uk/iplayer/categories/science-and-nature/a-z?sort=atoz",
+	"sport" : "https://www.bbc.co.uk/iplayer/categories/sport/a-z?sort=atoz",
 }
 
 func newCategory(name string, np NextPager) *Category {
