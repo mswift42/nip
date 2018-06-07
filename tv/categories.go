fcdc6310347a81e7aa26dac48e6dda863023128a
mswift42 <martin.haesler@gmail.com>
2018-06-07 06:28:35 +0100

    convert caturls to type map[string]Pager

--- tv/categories.go
+++ tv/categories.go
@@ -5,22 +5,22 @@ type Category struct {
 	Programmes []*Programme
 }
 
-var caturls = map[string]string{
-	"films" : "https://www.bbc.co.uk/iplayer/categories/films/a-z?sort=atoz&page=1",
-	"food" : "https://www.bbc.co.uk/iplayer/categories/food/a-z?sort=atoz&page=1",
-	"comedy" : "https://www.bbc.co.uk/iplayer/categories/comedy/a-z?sort=atoz",
-	"crime" : "https://www.bbc.co.uk/iplayer/categories/drama-crime/a-z?sort=atoz",
-	"classic&period" : "https://www.bbc.co.uk/iplayer/categories/drama-classic-and-period/a-z?sort=atoz",
-	"sc-fi&fantasy" : "https://www.bbc.co.uk/iplayer/categories/drama-sci-fi-and-fantasy/a-z?sort=atoz",
-	"documentaries" : "https://www.bbc.co.uk/iplayer/categories/documentaries/a-z?sort=atoz",
-	"arts" : "https://www.bbc.co.uk/iplayer/categories/arts/a-z?sort=atoz",
-	"entertainment" : "https://www.bbc.co.uk/iplayer/categories/entertainment/a-z?sort=atoz",
-	"history" : "https://www.bbc.co.uk/iplayer/categories/history/a-z?sort=atoz",
-	"lifestyle" : "https://www.bbc.co.uk/iplayer/categories/lifestyle/a-z?sort=atoz",
-	"music" : "https://www.bbc.co.uk/iplayer/categories/music/a-z?sort=atoz",
-	"news"  : "https://www.bbc.co.uk/iplayer/categories/news/a-z?sort=atoz",
-	"science&nature" : "https://www.bbc.co.uk/iplayer/categories/science-and-nature/a-z?sort=atoz",
-	"sport" : "https://www.bbc.co.uk/iplayer/categories/sport/a-z?sort=atoz",
+var caturls = map[string]Pager{
+	"films" : BeebURL("https://www.bbc.co.uk/iplayer/categories/films/a-z?sort=atoz&page=1"),
+	"food" : BeebURL("https://www.bbc.co.uk/iplayer/categories/food/a-z?sort=atoz&page=1"),
+	"comedy" : BeebURL("https://www.bbc.co.uk/iplayer/categories/comedy/a-z?sort=atoz"),
+	"crime" : BeebURL("https://www.bbc.co.uk/iplayer/categories/drama-crime/a-z?sort=atoz"),
+	"classic&period" : BeebURL("https://www.bbc.co.uk/iplayer/categories/drama-classic-and-period/a-z?sort=atoz"),
+	"sc-fi&fantasy" : BeebURL("https://www.bbc.co.uk/iplayer/categories/drama-sci-fi-and-fantasy/a-z?sort=atoz"),
+	"documentaries" : BeebURL("https://www.bbc.co.uk/iplayer/categories/documentaries/a-z?sort=atoz"),
+	"arts" : BeebURL("https://www.bbc.co.uk/iplayer/categories/arts/a-z?sort=atoz"),
+	"entertainment" : BeebURL("https://www.bbc.co.uk/iplayer/categories/entertainment/a-z?sort=atoz"),
+	"history" : BeebURL("https://www.bbc.co.uk/iplayer/categories/history/a-z?sort=atoz"),
+	"lifestyle" : BeebURL("https://www.bbc.co.uk/iplayer/categories/lifestyle/a-z?sort=atoz"),
+	"music" : BeebURL("https://www.bbc.co.uk/iplayer/categories/music/a-z?sort=atoz"),
+	"news"  : BeebURL("https://www.bbc.co.uk/iplayer/categories/news/a-z?sort=atoz"),
+	"science&nature" : BeebURL("https://www.bbc.co.uk/iplayer/categories/science-and-nature/a-z?sort=atoz"),
+	"sport" : BeebURL("https://www.bbc.co.uk/iplayer/categories/sport/a-z?sort=atoz"),
 }
 
 func newCategory(name string, np NextPager) *Category {
