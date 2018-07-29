f15a448f61318c017d3d2352440a07f34f2092d7
mswift42 <martin.haesler@gmail.com>
2018-07-29 14:04:11 +0100

    Update TODO's.

--- cl/cl.go
+++ cl/cl.go
@@ -27,6 +27,7 @@ func extractIndex(c *cli.Context) (int, error) {
 	return int(index), nil
 }
 
+// TODO - implement refresh cli command.
 // TODO - add helptext for cli commands.
 // TODO - split SaveDb into more functions for saving of db and refreshing .
 
