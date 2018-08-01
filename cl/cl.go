2dce9885de36533edb684c3d4e38d00bf47d9f3c
mswift42 <martin.haesler@gmail.com>
2018-08-01 06:14:09 +0100

    Override Help Template.

--- cl/cl.go
+++ cl/cl.go
@@ -42,11 +42,27 @@ func InitCli() *cli.App {
 	app := cli.NewApp()
 	app.Setup()
 	app.Name = "nip"
+	app.Version = "0.0.1"
+	app.Copyright = "2018 (c) Martin Haesler"
 	app.Usage = "search for iplayer tv programmes."
-	cli.AppHelpTemplate = fmt.Sprintf(`%s
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
 COMMANDS:
-{{range .Commands}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}
-`, cli.AppHelpTemplate)
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+COPYRIGHT:
+   {{.Copyright}}
+VERSION:
+   {{.Version}}
+`
 	app.Commands = []cli.Command{
 		{
 			Name:     "list",
