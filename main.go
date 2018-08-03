d693fade3745293059d64d8496c2c4f0e35da5d5
mswift42 <martin.haesler@gmail.com>
2018-08-03 10:35:13 +0100

    Delete outcommented lines.

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"github.com/mswift42/nip/cl"
-	"os"
 	"log"
+	"os"
+
+	"github.com/mswift42/nip/cl"
 )
 
 func main() {
-	//url := "https://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz&page=1"
-	//doc := tv.RemoteDocumentLoader(url)
-	//nmc := tv.NewMainCategory(doc)
-	//fmt.Println(nmc)
-	//progs := nmc.Programmes()
-	//for _, i := range progs {
-	//	fmt.Println(i)
-	//}
-	//foodurl := "https://www.bbc.co.uk/iplayer/categories/food/all?sort=atoz&page=1"
-	//doc = tv.RemoteDocumentLoader(foodurl)
-	//nmc = tv.NewMainCategory(doc)
-	//progs = nmc.Programmes()
-	//for _, i := range progs {
-	//	fmt.Println(i)
-	//}
-	//tv.SaveDB()
 	app := cl.InitCli()
 	err := app.Run(os.Args)
 	if err != nil {
