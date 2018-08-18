37ff03afbc2118dc8645e8498b5312869d98bcc9
mswift42 <martin.haesler@gmail.com>
2018-08-18 09:47:47 +0100

    fix windows open url command.

--- cl/cl.go
+++ cl/cl.go
@@ -113,7 +113,7 @@ VERSION:
 				case "darwin":
 					err = exec.Command("open", url).Start()
 				case "windows":
-					err = exec.Command("cmd", "/c", url).Start()
+					err = exec.Command("cmd", "/c", "start", url).Start()
 				default:
 					fmt.Println("Unsupported platform.")
 				}
