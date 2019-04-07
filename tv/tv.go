12694d00b30db943329cc7256dc353c1f49aa4de
mswift42 <martin.haesler@gmail.com>
2019-04-07 12:37:04 +0100

    add boxSet method.

--- tv/tv.go
+++ tv/tv.go
@@ -236,6 +236,14 @@ func (id *iplayerDocument) programPages(selres []*iplayerSelectionResult) []Page
 	return urls
 }
 
+func (id *iplayerDocument) boxSet() []Pager {
+	var urls []Pager
+	id.doc.Find(".series-nav__button > a").Each(func(i int, s *goquery.Selection) {
+		urls = append(urls, BeebURL(s.AttrOr("href", "")))
+	})
+	return urls
+}
+
 // RelatedLink represents a link entry on a programme's root home page.
 // It consists of a title, e.g. IMDB, and the url, e.g.
 // https://www.imdb.com/title/tt0146316/?ref_=ttfc_fc_tt
